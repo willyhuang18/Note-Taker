86d98c7fdf8b095338e63550a16c2b909b967804
wiily <willyhuang18@gmail.com>
2022-01-30 21:36:06 -0800

    trying both get and post route

--- Develop/routes/router.js
+++ Develop/routes/router.js
@@ -5,8 +5,18 @@ const data = require('../db/data');
 
 //get request
 router.get('/notes', (req, res)=>{
+  console.info(`${req.method} request received for feedback`);
     data
     .read()
     .then(notes => res.json(notes))
     .catch(err=> res.status(404).json(err))
+})
+
+//post request
+router.post('/notes', (req, res)=>{
+  console.info(`${req.method} request received to submit feedback`);
+  data
+  .addNote()
+  .then(notes => res.json(notes))
+  .catch(err=> res.status(404).json(err))
 })
