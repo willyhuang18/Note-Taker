c34c7fe7390e83e363e7d64546dcfd4d026a2775
wiily <willyhuang18@gmail.com>
2022-01-30 21:32:29 -0800

    trying the get request

--- Develop/routes/router.js
+++ Develop/routes/router.js
@@ -2,3 +2,11 @@
 const router = require('express').Router();
 //import data.js
 const data = require('../db/data');
+
+//get request
+router.get('/notes', (req, res)=>{
+    data
+    .read()
+    .then(notes => res.json(notes))
+    .catch(err=> res.status(404).json(err))
+})
