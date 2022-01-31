91686a02edac6d63ad0225ced4d57f7a7969a27d
wiily <willyhuang18@gmail.com>
2022-01-30 22:25:55 -0800

    using the static from 'public'

--- Develop/server.js
+++ Develop/server.js
@@ -10,3 +10,6 @@ const PORT = process.env.PORT || 3001
 // Middleware for parsing application/json and urlencoded data
 app.use(express.json())
 app.use(express.urlencoded({extended: true}));
+
+//using the static from 'public'
+app.use(express.static('public'));
