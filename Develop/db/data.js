aa85e88ba5bef5069b0d577170ae05333a2553aa
wiily <willyhuang18@gmail.com>
2022-01-30 20:36:47 -0800

    using promisify() method

--- Develop/db/data.js
+++ Develop/db/data.js
@@ -3,3 +3,9 @@ const util =require('util');
 const fs = require('fs');
 //provide a consistent format for any unique ID we use for our data.
 const {v4:uuidv4}=require('uuid');
+
+//using promisify() method to takes a function as an input that follows the common Node. js callback style,
+const readNote = util.promisify(fs.readFile);
+const writeNote = util.promisify(fs.writeFile);
+
+
