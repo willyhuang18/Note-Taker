2e4efe3773e1a7da4abaf30d50d8b1e95251e5d2
wiily <willyhuang18@gmail.com>
2022-01-30 20:52:17 -0800

    create addNote() for adding more note

--- Develop/db/data.js
+++ Develop/db/data.js
@@ -16,4 +16,17 @@ class Save{
 
     //return the note to the fs.readFile method
     read(){return readNote('./Develop/db/db.json', 'utf8')};
+
+    //create addNote() for adding more note
+    addNote(note){
+        //const data from db.json as argument 
+        const {title, text} = note;
+        //give condition to check if there is input
+        if(!title || !text){
+            console.error('Please fill in the title and text!');
+        }
+        //make a new variable for the new note with the id 
+        const newNote = {title, text, id: uuidv4()};
+        return newNote;
+    }
 }
