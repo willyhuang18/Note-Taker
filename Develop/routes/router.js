60aec005a407be710d09673ddd4425aaf7e92b13
wiily <willyhuang18@gmail.com>
2022-01-30 21:58:43 -0800

    fixing the bug in router

--- Develop/routes/router.js
+++ Develop/routes/router.js
@@ -16,7 +16,7 @@ router.get('/notes', (req, res)=>{
 router.post('/notes', (req, res)=>{
   console.info(`${req.method} request received to submit feedback`);
   data
-  .addNote()
+  .addNote(req.body)
   .then(notes => res.json(notes))
   .catch(err=> res.status(404).json(err))
 })
