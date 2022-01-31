db3e9fe1978a012d1e06776a5fd6cfa33dfe04d4
wiily <willyhuang18@gmail.com>
2022-01-30 22:16:08 -0800

    something wrong with the router get notes

--- Develop/routes/html.js
+++ Develop/routes/html.js
@@ -7,3 +7,7 @@ const path = require('path');
 router.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, '../public/index.html'));
 })
+
+router.get('/notes', (req, res)=>{
+    res.sendFile(path.join(__dirname,'../public/notes.html'));
+})
