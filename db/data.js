e8ed3a7a6888c0f385fa16b133335400ac591f0b
wiily <willyhuang18@gmail.com>
2022-01-31 17:15:37 -0800

    change path

--- db/data.js
+++ db/data.js
@@ -2,7 +2,7 @@
 const util =require('util');
 const fs = require('fs');
 //provide a consistent format for any unique ID we use for our data.
-const uuid = require('./helper/uuid');
+const uuid = require('../helper/uuid');
 
 //using promisify() method to takes a function as an input that follows the common Node. js callback style,
 const readNote = util.promisify(fs.readFile);
