91bd78ac6dece74f484649f06b5e8d95ab800b43
wiily <willyhuang18@gmail.com>
2022-01-31 16:12:42 -0800

    change back the html path

--- Develop/routes/html.js
+++ Develop/routes/html.js
@@ -5,11 +5,11 @@ const path = require('path');
 
 //get request 
 router.get('/notes', (req, res)=>{
-    res.sendFile(path.join(__dirname,'./public/notes.html'));
+    res.sendFile(path.join(__dirname,'../public/notes.html'));
 })
 
 router.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 })
 
 
